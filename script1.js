b194aae9726cf2aaa88a8358b434fa4e019b7ba6
Carl0M4r1n <carlo.marin02@inacapmail.cl>
2025-05-26 12:14:02 -0400

    commit.2

--- script1.js
+++ script1.js
@@ -1,2 +1,41 @@
 //validar nombre, debe contener solo letras y no ser vacio
 //validar correo: Debe tener solo un @ y terminar en .cl
+
+function validar(nombre,email){
+    nombre = vNombre,
+    vNombre = LongitudNombre,
+    email = vEmail
+    vEmail = ValidacionEmail
+
+
+    if(vNombre == "exito")
+        if (vEmail == "exito")
+            push[nombre , email]
+        else
+            console.log ("correo no es correcto")
+    else
+        console.log("nombre no es correcto")
+}
+
+function LongitudNombre(){
+    if (vNombre < 1)
+        return "error"
+        console.log("Nombre nesita letras")
+    elif (vNombre >= 1)
+        return "exito"
+}
+
+function ValidacionEmail(){
+    if ("@" != 1)
+        return "error"
+        console.log("solo puede haber 1 @")
+    elif (vNombre >= 1)
+        return "exito"
+}
+
+function eliminar(nombre,index){
+    if (index != indice)
+        return nombre
+    else
+        return ""
+}
